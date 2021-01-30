d027a8f5c6a62d77c59efbcab8d6124a72032f96
Alison Cordeiro <acordeiro@gmail.com>
2021-01-30 14:19:37 -0800

    Final edits

--- produce_summary.py
+++ produce_summary.py
@@ -1,17 +1,16 @@
 def print_deliveries(file_list):
     for index, filename in enumerate(file_list):
         print(f"Day {index + 1}")
-        the_file = open(filename)
-        for line in the_file:
-            line = line.rstrip()
-            words = line.split('|')
-            
-            melon = words[0]
-            count = words[1]
-            amount = words[2]
-            
-            print(f"Delivered {count} {melon}s for a total of ${amount}")
-        the_file.close()
+        with open(filename) as the_file:
+            for line in the_file:
+                line = line.rstrip()
+                words = line.split('|')
+                
+                melon = words[0]
+                count = words[1]
+                amount = words[2]
+                
+                print(f"Delivered {count} {melon}s for a total of ${amount}")
         print("\n")
 
 
